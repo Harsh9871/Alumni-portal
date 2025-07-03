14c5bee6335bd67e9f00f48865ca4132231098c0
Harsh9871 <raithathaproduction@gmail.com>
2025-07-04 01:09:51 +0530

    Add job routes, controller, and service for job management

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const app = express();
 const authRoutes = require('./routes/auth.routes');
 const uploadRoutes = require('./routes/upload.routes');
 const  userRoutes = require('./routes/user.routes');
+const jobsRoutes = require('./routes/jobs.routes');
 // Middleware
 app.use(express.json());
 
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use('/auth', authRoutes);
 app.use('/api', uploadRoutes);
 app.use('/user', userRoutes);
+app.use('/jobs', jobsRoutes)
 
 // ✅ Serve /public folder statically
 app.use('/public', express.static(path.join(__dirname, './public')));
